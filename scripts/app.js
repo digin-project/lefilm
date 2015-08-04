98ed2a39b55c85ae277d63d2250bba1e29ad77f7
bltnico <bellot.n@gmail.com>
2015-08-04 12:40:24 +0200

    Add load more

--- scripts/app.js
+++ scripts/app.js
@@ -10,4 +10,14 @@ $(document).ready(function() {
         $(this).children('.film-infos').removeClass('show');
     });
 
+    (function() {
+        var dummy = $('.row-film:first-child').parent().html();
+        var grid = $('#grid');
+
+        $('#load-more').on('click', function() {
+            grid.append(dummy);
+        });
+
+    })();
+
 });
