d37e0df77725003d756efa02d9ce8f5b32672e8a
bltnico <bellot.n@gmail.com>
2015-08-06 11:08:25 +0200

    Better responsive

--- scripts/app.js
+++ scripts/app.js
@@ -13,11 +13,38 @@ $(document).ready(function() {
         }
     }, ".film-wrapper");
 
+
+    toggleSearch();
+    $(window).resize(function() { toggleSearch(); });
+
+    function toggleSearch() {
+        if($(document).width() < 765) {
+
+            var input = $('.nav.navbar-search input'),
+                close = '#close-search',
+                label = '.nav.navbar-search label';
+
+            $(document).on('click mousedown', label, function() {
+                $(this).parent().css('background','#fff');
+                $(this).addClass('hide');
+                $('.__toggle').addClass('show');
+                input.focus();
+            });
+
+            $(document).on('click mousedown', close, function() {
+                $('.nav.navbar-search').css('background','transparent');
+                $(label).addClass('show');
+                $('.__toggle').toggleClass('show');
+
+            });
+        }
+    }
+
     (function() {
         var dummy = $('.row-film:first-child').parent().html();
         var grid = $('#grid');
 
-        $('#load-more').on('click', function() {
+        $('#load-more').on('click mousedown', function() {
             grid.append(dummy);
         });
 
