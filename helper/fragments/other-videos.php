5ff43da2c4a3562695737feffc75c9b71b622299
bltnico <bellot.n@gmail.com>
2015-08-10 15:51:31 +0200

    Add minions thumb

--- helper/fragments/other-videos.php
+++ helper/fragments/other-videos.php
@@ -4,9 +4,9 @@
         <li class="row">
             <div class="col-lg-5 col-md-5 col-sm-2 col-xs-5 thumb"></div>
             <div class="col-lg-7 col-md-7 col-sm-10 col-xs-7">
-                <h4>Solace</h4>
+                <h4>Les Minions</h4>
                 <p>par <span class="black">Teaser-Trailer.com</span></p>
-                <p>869 vues</p>
+                <p>13 162 vues</p>
             </div>
         </li>
         <li class="row">
