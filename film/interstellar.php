56159260829c435f06766e3a838e556d534618ed
Lucas Soler <solerl@estei.fr>
2015-08-10 15:20:17 +0200

    footer page film

--- film/interstellar.php
+++ film/interstellar.php
@@ -27,7 +27,165 @@
                 </div>
             </div>
         </div>
+        <div class="recommendations">
+            <div class="container-fluid ">
+                <div class="row">
+                    <div class="col-lg-5">
+                        <div class="clear"></div>
+                        <h2>Divergente, toute la sage !</h2>
+                        <div class="film films-form-right">
+                            <div class="first">
+                                <div class="image" style="background-image:url('/images/dummy/divergente.jpg');"><div class="internShadow"></div></div>
+                            </div>
+                            <div>
+                                <div class="image" style="background-image:url('/images/dummy/divergente2.jpg');"><div class="internShadow"></div></div>
+                            </div>
+                        </div>
+                     </div>
+                    <div class="col-lg-5">
+                        <div class="clear"></div>
+                        <h2>Prochainement dans les salles</h2>
+                        <div class="film films-form-left">
+                            <div>
+                                <div class="image" style="background-image:url('/images/dummy/amy.jpg');"><div class="internShadow"></div></div>
+                            </div>
+                            <div>
+                                <div class="image" style="background-image:url('/images/dummy/insidious3.jpg');"><div class="internShadow"></div></div>
+                            </div>
+                            <div>
+                                <div class="image" style="background-image:url('/images/dummy/minions.jpg');"><div class="internShadow"></div></div>
+                            </div>
+                            <div>
+                                <div class="image" style="background-image:url('/images/dummy/magicmike.jpg');"><div class="internShadow"></div></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-lg-5">
+                        <div class="clear"></div>
+                        <h3>Les sorties de la semaine</h3>
+                        <div class="slider">
+                            <div class="film films-form-right show">
+                                <div class="first">
+                                    <div class="image" style="background-image:url('/images/dummy/lesprofs2.jpg');"><div class="internShadow"></div></div>
+                                </div>
+                                <div>
+                                    <div class="image" style="background-image:url('/images/dummy/terminator.jpg');"><div class="internShadow"></div></div>
+                                </div>
+                                <div class="last">
+                                    <div class="image" style="background-image:url('/images/dummy/taleoftales.jpg');"><div class="internShadow"></div></div>
+                                </div>
+                            </div>
+                            <div class="film films-form-right hide">
+                                <div class="first">
+                                    <div class="image" style="background-image:url('/images/dummy/lesprofs2.jpg');"><div class="internShadow"></div></div>
+                                </div>
+                                <div>
+                                    <div class="image" style="background-image:url('/images/dummy/terminator.jpg');"><div class="internShadow"></div></div>
+                                </div>
+                                <div class="last">
+                                    <div class="image" style="background-image:url('/images/dummy/taleoftales.jpg');"><div class="internShadow"></div></div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="clear"></div>
+                        <div class="actions">
+                            <a href="" class="see-more">En voir plus ...</a>
 
+                            <div class="controls">
+                                <a href="javascript:prev();"><i class="fa fa-arrow-circle-left"></i></a>
+                                <ul>
+                                    <li><a href="javascript:changeThumb(0);" class="active"><i class="fa fa-circle"></i></i></a></li>
+                                    <li><a href="javascript:changeThumb(1);"><i class="fa fa-circle"></i></a></li>
+                                </ul>
+                                <a href="javascript:next();"><i class="fa fa-arrow-circle-right"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-5">
+                        <div class="clear"></div>
+                        <h3>Avec les mêmes acteurs</h3>
+                        
+                        <div class="film films-form-left">
+                            <div>
+                                <div class="image" style="background-image:url('/images/dummy/amy.jpg');"><div class="internShadow"></div></div>
+                            </div>
+                            <div>
+                                <div class="image" style="background-image:url('/images/dummy/insidious3.jpg');"><div class="internShadow"></div></div>
+                            </div>
+                            <div>
+                                <div class="image" style="background-image:url('/images/dummy/horsdutemps.jpg');"><div class="internShadow"></div></div>
+                            </div>
+                            <div>
+                                <div class="image" style="background-image:url('/images/dummy/magicmike.jpg');"><div class="internShadow"></div></div>
+                            </div>
+                            <div>
+                                <div class="image" style="background-image:url('/images/dummy/holiday.jpg');"><div class="internShadow"></div></div>
+                            </div>
+                            <div>
+                                <div class="image" style="background-image:url('/images/dummy/tattoo.jpg');"><div class="internShadow"></div></div>
+                            </div>
+                        </div>
+                        <div class="clear"></div>
+                        <div class="actions">
+
+                            <div class="controls">
+                                <a href="javascript:prev();"><i class="fa fa-arrow-circle-left"></i></a>
+                                <ul>
+                                    <li><a href="javascript:changeThumb(0);" class="active"><i class="fa fa-circle"></i></i></a></li>
+                                    <li><a href="javascript:changeThumb(1);"><i class="fa fa-circle"></i></a></li>
+                                </ul>
+                                <a href="javascript:next();"><i class="fa fa-arrow-circle-right"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-lg-5">
+                        <div class="clear"></div>
+                        <h3>Vous aimez ce réalisateur ? Vous aimerez aussi :</h3>    
+                        <div class="slider">
+                            <div class="film films-form-right show">
+                                <div class="first">
+                                    <div class="image" style="background-image:url('/images/dummy/flightplan.jpg');"><div class="internShadow"></div></div>
+                                </div>
+                                <div>
+                                    <div class="image" style="background-image:url('/images/dummy/tattoo.jpg');"><div class="internShadow"></div></div>
+                                </div>
+                                <div class="last">
+                                    <div class="image" style="background-image:url('/images/dummy/horsdutemps.jpg');"><div class="internShadow"></div></div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-5">
+                        <div class="clear"></div>
+                        <h3>On parie que vous aimez aussi</h3>
+                        <div class="film films-form-left">
+                            <div>
+                                <div class="image" style="background-image:url('/images/dummy/hamlet.jpg');"><div class="internShadow"></div></div>
+                            </div>
+                            <div>
+                                <div class="image" style="background-image:url('/images/dummy/3royaumes.jpg');"><div class="internShadow"></div></div>
+                            </div>
+                            <div>
+                                <div class="image" style="background-image:url('/images/dummy/holiday.jpg');"><div class="internShadow"></div></div>
+                            </div>
+                            <div>
+                                <div class="image" style="background-image:url('/images/dummy/holdup.jpg');"><div class="internShadow"></div></div>
+                            </div>
+                            <div>
+                                <div class="image" style="background-image:url('/images/dummy/divergente.jpg');"><div class="internShadow"></div></div>
+                            </div>
+                            <div>
+                                <div class="image" style="background-image:url('/images/dummy/magicmike.jpg');"><div class="internShadow"></div></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
         <?php require "../helper/fragments/footer.php"; ?>
         <?php require "../helper/fragments/script.php"; ?>
     </body>
--- helper/fragments/head.php
+++ helper/fragments/head.php
@@ -6,7 +6,7 @@
     <!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
 
     <!-- LIBRARIES -->
-    <link href='http://fonts.googleapis.com/css?family=Roboto:400,500' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Roboto:400,500,300' rel='stylesheet' type='text/css'>
     <!-- <link rel="stylesheet" href="libraries/bootstrap/css/bootstrap.min.css"> -->
     <!-- Il manque un truc css pour le burger, à trouver ... -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css">
@@ -14,4 +14,5 @@
     <link rel="stylesheet" href="/styles/app.css">
     <link rel="stylesheet" href="/styles/footer.css">
     <link rel="stylesheet" href="/styles/mobile.css">
+    <link rel="stylesheet" href="/styles/film.css">
 </head>
