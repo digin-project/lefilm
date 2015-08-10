02e0be3c8e1f47394c7fb0273ff893de6fabc555
bltnico <bellot.n@gmail.com>
2015-08-10 15:58:23 +0200

    Better other video template

--- helper/fragments/other-videos.php
+++ helper/fragments/other-videos.php
@@ -6,31 +6,31 @@
             <div class="col-lg-7 col-md-7 col-sm-10 col-xs-7">
                 <h4>Les Minions</h4>
                 <p>par <span class="black">Teaser-Trailer.com</span></p>
+                <p>27 803 vues</p>
+            </div>
+        </li>
+        <li class="row">
+            <div class="col-lg-5 col-md-5 col-sm-2 col-xs-5 thumb" style="background-image: url('/images/dummy/fantastique.jpg');"></div>
+            <div class="col-lg-7 col-md-7 col-sm-10 col-xs-7">
+                <h4>Les 4 Fantastiques</h4>
+                <p>par <span class="black">Teaser-Trailer.com</span></p>
                 <p>13 162 vues</p>
             </div>
         </li>
         <li class="row">
-            <div class="col-lg-5 col-md-5 col-sm-2 col-xs-5 thumb"></div>
+            <div class="col-lg-5 col-md-5 col-sm-2 col-xs-5 thumb" style="background-image: url('/images/dummy/ant-thumb.jpg');"></div>
             <div class="col-lg-7 col-md-7 col-sm-10 col-xs-7">
-                <h4>Solace</h4>
+                <h4>Ant-man</h4>
                 <p>par <span class="black">Teaser-Trailer.com</span></p>
-                <p>869 vues</p>
+                <p>12 996 vues</p>
             </div>
         </li>
         <li class="row">
-            <div class="col-lg-5 col-md-5 col-sm-2 col-xs-5 thumb"></div>
+            <div class="col-lg-5 col-md-5 col-sm-2 col-xs-5 thumb" style="background-image: url('/images/dummy/prof-thumb.jpg');"></div>
             <div class="col-lg-7 col-md-7 col-sm-10 col-xs-7">
-                <h4>Solace</h4>
+                <h4>Les Profs 2</h4>
                 <p>par <span class="black">Teaser-Trailer.com</span></p>
-                <p>869 vues</p>
-            </div>
-        </li>
-        <li class="row">
-            <div class="col-lg-5 col-md-5 col-sm-2 col-xs-5 thumb"></div>
-            <div class="col-lg-7 col-md-7 col-sm-10 col-xs-7">
-                <h4>Solace</h4>
-                <p>par <span class="black">Teaser-Trailer.com</span></p>
-                <p>869 vues</p>
+                <p>8 101 vues</p>
             </div>
         </li>
     </ul>
