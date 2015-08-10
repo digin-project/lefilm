3d047c2d760e18a18be7f463715521b6b26331ba
bltnico <bellot.n@gmail.com>
2015-08-10 12:12:56 +0200

    fezihfzei

--- film/interstellar.php
+++ film/interstellar.php
@@ -6,9 +6,29 @@
         require "../helper/fragments/head.php";
     ?>
     <body>
+
         <header>
             <?php require "../helper/fragments/top-header.php"; ?>
+            <nav class="sub-nav">
+                <div class="container-fluid">
+                    <small>Aventure - 2014</small>
+                    <h1>Interstellar</h1>
+                </div>
+            </nav>
         </header>
+
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-lg-8">
+                    <p>Alors que la Terre se meurt, une équipe d'astronautes franchit un trou de ver apparu près de Saturne conduisant à une autre galaxie. Ceci dans le but d'explorer un nouveau système stellaire et d'espérer trouver une nouvelle planète habitable pour l'humanité, dans l'espoir de la sauver.</p>
+                </div>
+                <div class="col-lg-4">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                </div>
+            </div>
+        </div>
+
         <?php require "../helper/fragments/footer.php"; ?>
+        <?php require "../helper/fragments/script.php"; ?>
     </body>
 </html>
--- index.php
+++ index.php
@@ -307,28 +307,6 @@
     </div>
 
     <?php require "/helper/fragments/footer.php"; ?>
-
-    <!-- Google Analytics: change UA-XXXXX-X to be your site's ID -->
-    <script>
-        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-        })(window,document,'script','http//www.google-analytics.com/analytics.js','ga');
-
-        ga('create', 'UA-XXXXX-X');
-        ga('send', 'pageview');
-    </script>
-    <!-- LIBRARIES -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.0.0-alpha1/jquery.min.js"></script>
-    <script src="libraries/bootstrap/js/bootstrap.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/fastclick/1.0.6/fastclick.min.js"></script>
-    <script type="text/javascript">
-    if ('addEventListener' in document) {
-        document.addEventListener('DOMContentLoaded', function() {
-            FastClick.attach(document.body);
-        }, false);
-    }
-    </script>
-    <script src="scripts/app.min.js"></script>
+    <?php require "/helper/fragments/script.php"; ?>
 </body>
 </html>
