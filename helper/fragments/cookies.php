047d9c1270179239a9b5996930c8804d230e6f8d
bltnico <bellot.n@gmail.com>
2015-08-13 11:58:12 +0200

    Minor fix

--- helper/fragments/cookies.php
+++ helper/fragments/cookies.php
@@ -1,8 +1,8 @@
 <div id="cookies-container">
     <p>En poursuivant votre navigation sur ce site, vous acceptez l'utilisation de cookies</p>
     <div>
-        <button class="c-close">Fermer !</button>
-        <button class="c-more">Pour en savoir plus et paramétrer les cookies, cliquez ici.</button>
+        <button class="c-close">Fermer</button>
+        <button class="c-more">En savoir plus</button>
     </div>
 </div>
 <script type="text/javascript">document.getElementById("cookies-container").addEventListener("click", function(e) { return document.getElementById("cookies-container").style.display = "none"; });</script>
